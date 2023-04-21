3256172b7749fe0cc9b9e2a654bb50066d17585f
root <gowtham.123.8500@gmail.com>
2023-04-21 12:21:34 +0000

    Adding react coding 1 files to git repository

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="super-over-league">
+    <div>
+      <h1>Super Over League</h1>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      ></img>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
